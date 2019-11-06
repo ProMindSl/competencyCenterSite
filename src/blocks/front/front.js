61627fdf092631f045eee159f0d1db62fb9cca9f
ProMindSl <hot_peachy@hotmail.com>
2019-11-06 23:46:49 +0300

    add title to front

--- src/blocks/front/front.js
+++ src/blocks/front/front.js
@@ -11,3 +11,21 @@ $(document).scroll(function()
 {
     toggleFront();
 })
+
+$(document).ready(function()
+{
+	animateShowFrontTitle('.carousel-item.active > .carousel-caption > h3', 100);
+	animateShowFrontTitle('.carousel-item.active > .carousel-caption > p', 200);
+});
+
+function animateShowFrontTitle(slideID, fromX)
+{
+	TweenMax.from(slideID, 1, {x:fromX});
+	TweenMax.from(slideID, 1.5, {opacity:0});
+}
+
+/*$('.carousel-item').onshow(
+{
+	animateShowFrontTitle('.carousel-item.active > .carousel-caption > h3', 100);
+	animateShowFrontTitle('.carousel-item.active > .carousel-caption > p', 200);  
+});*/
